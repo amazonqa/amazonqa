e57c3838baf3be6cbf530278b93bced43d653571
Nitish Kulkarni <ni4lgi@gmail.com>
2018-02-15 22:38:43 -0500

    Basic parsing and data analysis notebook

---
+++ src/data_utils.py
@@ -0,0 +1,18 @@
+"""Utilities to parse and analyse data
+"""
+
+import pandas as pd
+import gzip
+
+def parse(path):
+  g = gzip.open(path, 'rb')
+  for l in g:
+    yield eval(l)
+
+def getDF(path):
+  i = 0
+  df = {}
+  for d in parse(path):
+    df[i] = d
+    i += 1
+  return pd.DataFrame.from_dict(df, orient='index')
