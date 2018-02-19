fc98f622be0f7c964f00ec67a21d9b35679cb8ff
Nitish Kulkarni <ni4lgi@gmail.com>
2018-02-19 02:10:05 -0500

    Consistent tabs

--- src/data_utils.py
+++ src/data_utils.py
@@ -25,17 +25,17 @@ def filepath(category, key):
     return path
 
 def parse(path):
-  g = gzip.open(path, 'rb')
-  for l in g:
-    yield eval(l)
+    g = gzip.open(path, 'rb')
+    for l in g:
+        yield eval(l)
 
 def getDF(path):
-  i = 0
-  df = {}
-  for d in parse(path):
+    i = 0
+    df = {}
+    for d in parse(path):
     df[i] = d
     i += 1
-  return pd.DataFrame.from_dict(df, orient='index')
+    return pd.DataFrame.from_dict(df, orient='index')
 
 def create_qa_review_tables(category):
     qa, reviews = [getDF(filepath(category, key)) for key in [QA, REVIEWS]]
@@ -73,21 +73,20 @@ def filterReviews(columns):
     return columns['reviews'].tolist()[:2]
 
 def preprocessReviewsDF(reviews_path):
-  reviews_df = getDF('../data/reviews_'+category+'_5.json.gz')
-  reviews_df['reviews'] = reviews_df[['reviewText', 'helpful', 'summary']].apply(convertRowToReviewJson, axis=1)
-
-  reviews_filtered_df = reviews_df.groupby('asin').apply(filterReviews).reset_index()
-  reviews_filtered_df.columns = ['asin', 'reviews']
-  return reviews_filtered_df
+    reviews_df = getDF('../data/reviews_'+category+'_5.json.gz')
+    reviews_df['reviews'] = reviews_df[['reviewText', 'helpful', 'summary']].apply(convertRowToReviewJson, axis=1)
+    
+    reviews_filtered_df = reviews_df.groupby('asin').apply(filterReviews).reset_index()
+    reviews_filtered_df.columns = ['asin', 'reviews']
+    return reviews_filtered_df
 
 def flatten(qa_reviews_df):
-  qa_reviews_flattend_array = []
-  
-  for index, row in qa_reviews_df.iterrows():
-    reviews = row['reviews']
-    review_list = [review['reviewText'] for review in reviews]
+    qa_reviews_flattend_array = []
 
-    questions = row['questions']
+    for index, row in qa_reviews_df.iterrows():
+        reviews = row['reviews']
+        review_list = [review['reviewText'] for review in reviews]
+        questions = row['questions']
 
     for question in questions:
       for answer in question['answers'][:2]:
@@ -97,20 +96,20 @@ def flatten(qa_reviews_df):
   return np.array(qa_reviews_flattend_array)
 
 def save_tables_for_all_categories():
-  """Processes .tar.gz files for all the categories
-  and saves qa & reviews tables in a .pickle format
-  """
-  for category in CATEGORIES:
-    create_qa_review_tables(category)
+    """Processes .tar.gz files for all the categories
+    and saves qa & reviews tables in a .pickle format
+    """
+    for category in CATEGORIES:
+        create_qa_review_tables(category)
 
 def preprocess(qa_path, reviews_path):
-  qa_df = getDF(qa_path)
-  reviews_df = preprocessReviewsDF(reviews_path)
+    qa_df = getDF(qa_path)
+    reviews_df = preprocessReviewsDF(reviews_path)
 
-  qa_reviews_df = pd.merge(qa_df, reviews_df, on=['asin', 'asin'])
+    qa_reviews_df = pd.merge(qa_df, reviews_df, on=['asin', 'asin'])
 
-  qa_reviews_flattend_array = flatten(qa_reviews_df)
-  np.save('qa_reviews', qa_reviews_flattend_array)
+    qa_reviews_flattend_array = flatten(qa_reviews_df)
+    np.save('qa_reviews', qa_reviews_flattend_array)
 
 # if __name__ == "__main__":
 #   category = 'Video_Games'
