93ca8080b518300a0f901f02ac8e1d46681a50ef
Raghuveer Chanda <raghuveer.chanda@gmail.com>
2018-02-19 02:07:49 -0500

    Added all Categories

--- src/data_utils.py
+++ src/data_utils.py
@@ -6,9 +6,41 @@ import pandas as pd
 import gzip
 import numpy as np
 
+AUTOMOTIVE = 'Automotive'
+BABY = 'Baby'
+BEAUTY = 'Beauty'
+CELL_PHONES_AND_ACCESSORIES = 'Cell_Phones_and_Accessories'
+CLOTHING_SHOES_AND_JEWELRY = 'Clothing_Shoes_and_Jewelry'
+ELECTRONICS = 'Electronics'
+GROCERY_AND_GOURMET_FOOD = 'Grocery_and_Gourmet_Food'
+HEALTH_AND_PERSONAL_CARE = 'Health_and_Personal_Care'
+HOME_AND_KITCHEN = 'Home_and_Kitchen'
+MUSICAL_INSTRUMENTS = 'Musical_Instruments'
+OFFICE_PRODUCTS = 'Office_Products'
+PATIO_LAWN_AND_GARDEN = 'Patio_Lawn_and_Garden'
+PET_SUPPLIES = 'Pet_Supplies'
+SPORTS_AND_OUTDOORS = 'Sports_and_Outdoors'
+TOOLS_AND_HOME_IMPROVEMENT = 'Tools_and_Home_Improvement'
+TOYS_AND_GAMES = 'Toys_and_Games'
 VIDEO_GAMES = 'Video_Games'
 
 CATEGORIES = [
+  AUTOMOTIVE,
+  BABY,
+  BEAUTY,
+  CELL_PHONES_AND_ACCESSORIES,
+  CLOTHING_SHOES_AND_JEWELRY,
+  ELECTRONICS,
+  GROCERY_AND_GOURMET_FOOD,
+  HEALTH_AND_PERSONAL_CARE,
+  HOME_AND_KITCHEN,
+  MUSICAL_INSTRUMENTS,
+  OFFICE_PRODUCTS,
+  PATIO_LAWN_AND_GARDEN,
+  PET_SUPPLIES,
+  SPORTS_AND_OUTDOORS,
+  TOOLS_AND_HOME_IMPROVEMENT,
+  TOYS_AND_GAMES,
   VIDEO_GAMES,
 ]
 
