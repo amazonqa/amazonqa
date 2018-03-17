df050ffa1843499c6a268748560859cef4b11ad2
Raghuveer Chanda <raghuveer.chanda@gmail.com>
2018-03-17 02:41:04 -0400

    pytorch example code

--- src/language_models/data.py
+++ src/language_models/data.py
@@ -36,58 +36,32 @@ class AnswersCorpus(object):
 		self.test = self.tokenize(test_path)
 
 	def tokenize(self, path):
+		print(path)
 		assert os.path.exists(path)
 
 		with open(path, 'rb') as f:
 			data = pd.read_pickle(f)
 
+		tokens = 0
 		for questionsList in data[C.QUESTIONS_LIST]:
 			for question in questionsList:
 				for answer in question[C.ANSWERS]:
 					line = answer[C.TEXT]
 					words = '<SOS>' + line.split() + '<EOS>'
+					tokens += len(words)
 					for word in words:
 						self.dicitionary.add_word(word)
 
-		answerIds_List = []
+		ids = torch.LongTensor(tokens)
+		token = 0
+
 		for questionsList in data[C.QUESTIONS_LIST]:
 			for question in questionsList:
 				for answer in question[C.ANSWERS]:
 					line = answer[C.TEXT]
 					words = '<SOS>' + line.split() + '<EOS>'
-					answerIds = []
 					for word in words:
-						answerIds.append(self.dicitionary.word2idx[word])
-					answerIds_List.append(np.array(answerIds))
+						ids[token]= self.dicitionary.word2idx[word]
+						token += 1
 
-		return np.array(answerIds_List)
-
-
-class ShuffleDataLoader(DataLoader):
-	def __init__(self, array, batch_size):
-		random.shuffle(array)
-		data = np.concatenate((array))
-		m = len(data) // batch_size
-
-		data = data[: m*batch_size+1]
-		self.inputs = data[:-1].reshape(batch_size, m).T
-		self.labels = data[1:].reshape(batch_size, m).T
-
-		self.inputs = torch.from_numpy(self.inputs).long()
-		self.labels = torch.from_numpy(self.labels).long()
-
-		if torch.cuda.is_available():
-			self.inputs = self.inputs.cuda()
-			self.labels = self.labels.cuda()
-
-	def __iter__(self):
-		for i in range(self.len):
-			start = i*self.seq_length
-			end = (i+1)*self.seq_length
-
-		yield (self.inputs[start:end], self.labels[start:end])
-
-	def __len__(self):
-		self.seq_length = 100
-		self.len = self.inputs.shape[0] // self.seq_length
-		return self.len
+		return ids
