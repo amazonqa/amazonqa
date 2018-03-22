9de8fdb258c1abb7adaaac747eef6af859dc3c7d
Raghuveer Chanda <raghuveer.chanda@gmail.com>
2018-03-21 22:23:45 -0400

    Data Loader Completed

--- src/language_models/data_loader.py
+++ src/language_models/data_loader.py
@@ -1,65 +1,108 @@
-from torch.utils.data import Dataset, DataLoader
-import pickle
-from constants import *
-import pandas as pd
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
+
 import numpy as np
-from tqdm import tqdm
+from operator import itemgetter, attrgetter
+from torch.utils.data import Dataset, DataLoader
 
 
-def get_data(category, split_type, typ):
-    #TODO questions and reviews 
-    return get_answers(category, split_type)
+class AmazonDataLoader(DataLoader):
 
+    def sortByLength(self, item):
+        if self.mode is "1":
+            return len(item)
 
-def get_answers(category, split_type):
-    answers = []
-    ansDict = Dictionary()
-    qadata = list(pickle.load(open(INPUT_DATA_PATH + '/' + \
-    split_type + '-' + category + '.pickle', 'rb'))['questionsList'])
+        elif self.mode is "2":
+            assert(len(item) == 2)
+            return len(item[0])
 
-    for q in tqdm(qadata):
-        for ans in q:
-            ans_ids = [] # my name is mansi # 13, 15, 12, 1
-            anstext = ans['text']
-            tokens = ['<sos>'] + anstext.split() + ['<eos>']
-            for token in tokens:
-                ansDict.add_word(token)
-                ans_ids.append(ansDict.word2idx[token])
-            answers.append(ans_ids)
+        elif self.mode is "3":
+            assert(len(item) == 3)
+            reviews = item[2]
+            max_len = 0
+            for review in reviews:
+                max_len = max(max_len, len(review))
+            return max_len
 
-    answers = np.array(answers)
-    # TODO check sorted 
-    indices = np.argsort([len(ans) for ans in answers])
-    answers = answers[indices]
-    return ansDict, answers
+    def __init__(self, data, mode, batch_size):
+        self.batch_size = batch_size
+        self.mode = mode
 
+        sorted(data, key=self.sortByLength, reverse=True)
+        self.data = data
 
-def get_batches(data):
-    # should be called per epoch
-    # data (np.array) should be sorted by length
-    num_splits = int ( np.ceil ( len(data) / batch_size ) )
-    batched_data = np.array_split( data, num_splits )
-    batched_data = np.array(batched_data)
-    np.random.shuffle(batched_data)
-    # TODO shuffle inside batch
-    #will pad by 0 '<pad>'
-    maxlen = max([len(item) for item in data])
-    padded_batched_data = np.array( [ np.pad( ass, (0, maxlen - len(ass) ), mode='constant')  for len in data] )
-    return padded_batched_data #array( [], [], [] )
+    def create_packed_qa(self, batch_data):
+        lengths = np.array([len(item) for item in batch_data])
+        max_len = max(lengths)
 
+        padded_data = np.array(
+            [np.pad(item, (0, max_len - len(item)), 'constant') for item in batch_data])
+        padded_data = torch.from_numpy(padded_data)
 
-class Dictionary(object):
-    # TODO: RARE WORDS HANDLING
-    def __init__(self):
-        self.word2idx = {'<pad>':0, '<sos>':1, '<eos>':2, '<unk>':3}
-        self.idx2word = {0:'<pad>', 1:'<sos>', 2:'<eos>', 3:'<unk>'}
-        self.num_tokens = 4
+        return (padded_data)
 
-    def add_word(self, word):
-        if not word in self.word2idx:
-            self.word2idx[word] = self.num_tokens
-            self.idx2word[self.num_tokens] = word
-            self.num_tokens += 1
+    def create_packed_reviews(self, review_data):
+        max_num_reviews = 0
+        for reviews in review_data:
+            max_num_reviews = max(max_num_reviews, len(reviews))
+
+        data = []
+        for i in range(max_num_reviews):
+            batch_data = []
+            for j in range(self.batch_size):
+                reviews = review_data[j]
+                if i < len(reviews):
+                    batch_data.append(reviews[i])
+                else:
+                    batch_data.append([0])
+            data.append(batch_data)
+
+        padded_data = []
+        for i in range(max_num_reviews):
+            batch_data = data[i]
+            lengths = [len(review) for review in batch_data]
+            max_len = max(lengths)
+
+            padded_batch_data = np.array(
+                [np.pad(item, (0, max_len - len(item)), 'constant') for item in batch_data])
+            padded_batch_data = torch.from_numpy(padded_batch_data)
+
+            padded_data.append(padded_batch_data)
+
+        return padded_data
+
+    def __iter__(self):
+        print(self.data)
+        self.num_batches = len(self.data) // self.batch_size
+        indices = np.arange(self.num_batches)
+        np.random.shuffle(indices)
+
+        for index in indices:
+            start = index * self.batch_size
+            end = (index + 1) * self.batch_size
+
+            batch_data = self.data[start:end]
+            assert(self.batch_size == len(batch_data))
+
+            if self.mode is "1":
+                answers = batch_data
+                packed_answers = self.create_packed_qa(answers)
+                yield (packed_answers)
+
+            elif self.mode is "2":
+                answers, questions = zip(*batch_data)
+                packed_answers = self.create_packed_qa(list(answers))
+                packed_questions = self.create_packed_qa(list(questions))
+                yield (packed_answers, packed_questions)
+
+            elif self.mode is "3":
+                answers, questions, reviews = zip(*batch_data)
+                packed_answers = self.create_packed_qa(list(answers))
+                packed_questions = self.create_packed_qa(list(questions))
+                packed_reviews = self.create_packed_reviews(list(reviews))
+                yield (packed_answers, packed_questions, packed_reviews)
 
     def __len__(self):
-        return len(self.word2idx)
+        return self.num_batches
