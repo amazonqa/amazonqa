3da5c9ea3dd3d140ec72837e7a81306fbf41af4a
Raghuveer Chanda <raghuveer.chanda@gmail.com>
2018-03-23 00:28:32 -0400

    storing ids instead of text

--- src/language_models/dataloader.py
+++ src/language_models/dataloader.py
@@ -12,28 +12,39 @@ class AmazonDataLoader(object):
 
     def sortByLength(self, item):
         if self.mode is "1":
-            return len(item)
+            answer = self.dataset.answers[item]
+            return len(answer)
 
         elif self.mode is "2":
             assert(len(item) == 2)
-            return len(item[0])
+            answer = self.dataset.answers[item[0]]
+            return len(answer)
 
         elif self.mode is "3":
             assert(len(item) == 3)
-            reviews = item[2]
+            reviewIds = item[2]
             max_len = 0
-            for review in reviews:
+            for reviewId in reviewIds:
+                review = self.dataset.reviews[reviewId]
                 max_len = max(max_len, len(review))
             return max_len
 
-    def __init__(self, data, mode, batch_size):
+
+    def __init__(self, dataset, mode, batch_size):
+        self.dataset = dataset
         self.batch_size = batch_size
         self.mode = mode
 
-        sorted(data, key=self.sortByLength, reverse=True)
-        self.data = data
+        self.dataset.data = sorted(self.dataset.data, key=self.sortByLength, reverse=True)
 
-    def create_packed_qa(self, batch_data):
+
+    def pad_answers(self, answerIds):
+        batch_data = []
+        for answerId in answerIds:
+            tokens = self.dataset.tokenize(self.dataset.answers[answerId])
+            ids = self.dataset.vocab.indices_from_sequence(tokens)
+            batch_data.append(ids)
+
         lengths = np.array([len(item) for item in batch_data])
         max_len = max(lengths)
 
@@ -43,7 +54,33 @@ class AmazonDataLoader(object):
 
         return (padded_data)
 
-    def create_packed_reviews(self, review_data):
+
+    def pad_questions(self, questionIds):
+        batch_data = []
+        for questionId in questionIds:
+            tokens = self.dataset.tokenize(self.dataset.questions[questionId])
+            ids = self.dataset.vocab.indices_from_sequence(tokens)
+            batch_data.append(ids)
+
+        lengths = np.array([len(item) for item in batch_data])
+        max_len = max(lengths)
+
+        padded_data = np.array(
+            [np.pad(item, (0, max_len - len(item)), 'constant') for item in batch_data])
+        padded_data = torch.from_numpy(padded_data)
+
+        return (padded_data)
+
+    def pad_reviews(self, reviewIdsList):
+        review_data = []
+        for reviewIds in reviewIdsList:
+            reviews = []
+            for reviewId in reviewIds:
+                tokens = self.dataset.tokenize(self.dataset.reviews[reviewId])
+                ids = self.dataset.vocab.indices_from_sequence(tokens)
+                reviews.append(ids)
+            review_data.append(reviews)
+
         max_num_reviews = 0
         for reviews in review_data:
             max_num_reviews = max(max_num_reviews, len(reviews))
@@ -73,9 +110,9 @@ class AmazonDataLoader(object):
 
         return padded_data
 
+
     def __iter__(self):
-        print(self.data)
-        self.num_batches = len(self.data) // self.batch_size
+        self.num_batches = len(self.dataset.data) // self.batch_size
         indices = np.arange(self.num_batches)
         np.random.shuffle(indices)
 
@@ -83,26 +120,27 @@ class AmazonDataLoader(object):
             start = index * self.batch_size
             end = (index + 1) * self.batch_size
 
-            batch_data = self.data[start:end]
+            batch_data = self.dataset.data[start:end]
             assert(self.batch_size == len(batch_data))
 
             if self.mode is "1":
-                answers = batch_data
-                packed_answers = self.create_packed_qa(answers)
-                yield (packed_answers)
+                answerIds = batch_data
+                paded_answers = self.pad_answers(answerIds)
+                yield (paded_answers)
 
             elif self.mode is "2":
-                answers, questions = zip(*batch_data)
-                packed_answers = self.create_packed_qa(list(answers))
-                packed_questions = self.create_packed_qa(list(questions))
-                yield (packed_answers, packed_questions)
+                answerIds, questionIds = zip(*batch_data)
+                paded_answers = self.pad_answers(list(answerIds))
+                padded_questions = self.pad_questions(list(questionIds))
+                yield (paded_answers, padded_questions)
 
             elif self.mode is "3":
-                answers, questions, reviews = zip(*batch_data)
-                packed_answers = self.create_packed_qa(list(answers))
-                packed_questions = self.create_packed_qa(list(questions))
-                packed_reviews = self.create_packed_reviews(list(reviews))
-                yield (packed_answers, packed_questions, packed_reviews)
+                answerIds, questionIds, reviewIds = zip(*batch_data)
+                paded_answers = self.pad_answers(list(answerIds))
+                padded_questions = self.pad_questions(list(questionIds))
+                padded_reviews = self.pad_reviews(list(reviewIds))
+                yield (paded_answers, padded_questions, padded_reviews)
+
 
     def __len__(self):
         return self.num_batches
