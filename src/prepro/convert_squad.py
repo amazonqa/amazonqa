e93bf645a5c67cc8f8c8843f58876d55c5d05dc9
Raghuveer Chanda <raghuveer.chanda@gmail.com>
2018-11-10 19:08:57 -0500

    convert_squad scripts

---
+++ src/prepro/convert_squad.py
@@ -0,0 +1,90 @@
+import argparse
+import string
+import math
+import json
+
+import pandas as pd
+import scipy.stats as st
+import nltk
+from nltk.corpus import stopwords
+from evaluator.evaluator import COCOEvalCap
+from operator import itemgetter, attrgetter
+
+
+def find_answer_spans(args, answer_span_lens, answers, context):
+	context = context.split(' ')
+
+	gold_answers_dict = {}
+	gold_answers_dict[0] = [answer["answerText"] for answer in answers]
+	answers = []
+	
+	for answer_span_len in answer_span_lens:
+		char_index = 0
+		for word_index in range(len(context)-answer_span_len):
+			span = ' '.join(context[word_index: word_index+answer_span_len])
+			
+			generated_answer_dict = {}
+			generated_answer_dict[0] = [span]
+			
+			score = COCOEvalCap.compute_scores(gold_answers_dict, generated_answer_dict)[args.evaluation_metric]
+			answers.append((score, {
+				'answer_start': char_index,
+				'text': span    
+			}))
+			char_index += (len(context[word_index]) + 1)
+
+	return [i[1] for i in sorted(answers, reverse=True, key=itemgetter(0))[:args.span_max_num]]
+
+
+def main(args):
+	stop_words = set(stopwords.words('english'))
+	answer_span_lens = range(args.span_min_len, args.span_max_len)
+	
+	wfp = open(args.output_file, 'w')
+	rfp = open(args.input_file, 'r')
+	qid = 0
+
+	for line in rfp:
+		row = json.loads(line)
+
+		if row["questionType"] == "yesno":
+			continue
+
+		reviews = row["review_snippets"]
+		context = ' '.join(reviews)
+
+		answers = row["answers"]
+		new_answers = find_answer_spans(args, answer_span_lens, answers, context)
+
+		qas = []
+		qas.append({
+			'id': qid,
+			'is_impossible': False,
+			'question': row["questionText"],
+			'answers': new_answers,
+			'human_answers': [answer["answerText"] for answer in answers],
+		})
+
+		wfp.write(json.dumps({
+				'context': context,
+				'qas': qas,
+		}) + '\n')
+
+		qid += 1
+
+	wfp.close()
+
+
+if __name__ == '__main__':
+	# parse arguments
+	argParser = argparse.ArgumentParser(description="Preprocess QA and Review Data")
+	argParser.add_argument("--input_file", type=str)
+	argParser.add_argument("--output_file", type=str)
+	argParser.add_argument("--span_min_len", type=int, default=2)
+	argParser.add_argument("--span_max_len", type=int, default=10)
+	argParser.add_argument("--span_max_num", type=int, default=5)
+	argParser.add_argument("--evaluation_metric", type=str, default="Bleu_2")
+
+	args = argParser.parse_args()
+	main(args)
+
---
+++ src/prepro/convert_squad.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+data_dir="../../data"
+parts=('train' 'val' 'test')
+parts=('val')
+num_process=1
+
+for part in "${parts[@]}"; do
+	echo "processing $part"
+	qar_all="$data_dir/$part-qar_all.jsonl"
+	
+	num_lines=($(wc -l $qar_all))
+	((num_splits = num_lines/$num_process))
+	echo "Total number of lines $num_lines"
+	
+	rm $data_dir/$part-split-*
+	split -l $num_splits $qar_all "$data_dir/$part-split-"
+	echo "Split Completed"
+
+	for input_file in $data_dir/$part-split-*; do
+		echo $input_file
+		output_file="$input_file.out"
+		python3 convert_squad.py --input_file $input_file --output_file $output_file &
+	done
+
+	wait
+	echo "Creation Completed"
+
+	qar_squad_all="$data_dir/$part-qar_squad_all.jsonl" 
+	for output_file in $data_dir/$part-split-*.out; do
+		cat $output_file
+	done > $qar_squad_all
+	echo "Cat Completed"
+
+	gshuf $qar_squad_all -o $qar_squad_all
+	rm $data_dir/$part-split-*
+	echo "Shuffle and Delete Completed"
+done
+
+
