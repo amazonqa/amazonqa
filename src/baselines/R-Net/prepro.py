0b86974aacf423f9e598b99c4744bcfebd4c92fb
Raghuveer Chanda <raghuveer.chanda@gmail.com>
2018-12-01 00:44:27 -0500

    prepro

--- src/baselines/R-Net/prepro.py
+++ src/baselines/R-Net/prepro.py
@@ -28,65 +28,91 @@ def convert_idx(text, tokens):
     return spans
 
 
-def process_file(filename, data_type, word_counter, char_counter):
+def process_file(config, data_type, word_counter, char_counter):
     print("Generating {} examples...".format(data_type))
-    examples = []
-    eval_examples = {}
+    
+    if data_type == "dev":
+        rfp = open(config.dev_file, 'rb')
+        wfp = open(config.dev_examples_file, 'wb')
+        wfp_eval = open(config.dev_eval_file, 'wb')
+    elif data_type == "test":
+        rfp = open(config.test_file, 'rb')
+        wfp = open(config.test_examples_file, 'wb')
+        wfp_eval = open(config.test_eval_file, 'wb')
+    elif data_type == "train":
+        rfp = open(config.train_file, 'rb')
+        wfp_eval = open(config.train_eval_file, 'wb')
+    else:
+        exit(1)
+
     total = 0
-    with open(filename, "r") as fh:
-        source = json.load(fh)
-        for article in tqdm(source["data"]):
-            for para in article["paragraphs"]:
-                context = para["context"].replace(
-                    "''", '" ').replace("``", '" ')
-                context_tokens = word_tokenize(context)
-                context_chars = [list(token) for token in context_tokens]
-                spans = convert_idx(context, context_tokens)
-                for token in context_tokens:
-                    word_counter[token] += len(para["qas"])
-                    for char in token:
-                        char_counter[char] += len(para["qas"])
-                for qa in para["qas"]:
-                    total += 1
-                    ques = qa["question"].replace(
-                        "''", '" ').replace("``", '" ')
-                    ques_tokens = word_tokenize(ques)
-                    ques_chars = [list(token) for token in ques_tokens]
-                    for token in ques_tokens:
-                        word_counter[token] += 1
-                        for char in token:
-                            char_counter[char] += 1
-                    y1s, y2s = [], []
-                    answer_texts = []
-                    for answer in qa["answers"]:
-                        answer_text = answer["text"]
-                        answer_start = answer['answer_start']
-                        answer_end = answer_start + len(answer_text)
-                        answer_texts.append(answer_text)
-                        answer_span = []
-                        for idx, span in enumerate(spans):
-                            if not (answer_end <= span[0] or answer_start >= span[1]):
-                                answer_span.append(idx)
-                        y1, y2 = answer_span[0], answer_span[-1]
-                        y1s.append(y1)
-                        y2s.append(y2)
-                    example = {"context_tokens": context_tokens, "context_chars": context_chars, "ques_tokens": ques_tokens,
-                               "ques_chars": ques_chars, "y1s": y1s, "y2s": y2s, "id": total}
-                    examples.append(example)
-                    eval_examples[str(total)] = {
-                        "context": context, "spans": spans, "answers": answer_texts, "uuid": qa["id"]}
-        random.shuffle(examples)
-        print("{} questions in total".format(len(examples)))
-    return examples, eval_examples
+
+    for line in tqdm(rfp):
+        para = json.loads(line)
+        context = para["context"].replace(
+            "''", '" ').replace("``", '" ')
+        context_tokens = word_tokenize(context)
+        context_chars = [list(token) for token in context_tokens]
+        spans = convert_idx(context, context_tokens)
+        for token in context_tokens:
+            word_counter[token] += len(para["qas"])
+            for char in token:
+                char_counter[char] += len(para["qas"])
+        for qa in para["qas"]:
+            total += 1
+            ques = qa["question"].replace(
+                "''", '" ').replace("``", '" ')
+            ques_tokens = word_tokenize(ques)
+            ques_chars = [list(token) for token in ques_tokens]
+            for token in ques_tokens:
+                word_counter[token] += 1
+                for char in token:
+                    char_counter[char] += 1
+            y1s, y2s = [], []
+            answer_texts = []
+            for answer in qa["answers"]:
+                answer_text = answer["text"]
+                answer_start = answer['answer_start']
+                answer_end = answer_start + len(answer_text)
+                answer_texts.append(answer_text)
+                answer_span = []
+                for idx, span in enumerate(spans):
+                    if not (answer_end <= span[0] or answer_start >= span[1]):
+                        answer_span.append(idx)
+                y1, y2 = answer_span[0], answer_span[-1]
+                y1s.append(y1)
+                y2s.append(y2)
+            
+            if data_type != "train":
+                example = {"context_tokens": context_tokens, "context_chars": context_chars, "ques_tokens": ques_tokens,
+                       "ques_chars": ques_chars, "y1s": y1s, "y2s": y2s, "id": total}
+                wfp.write(json.dumps(example) + '\n')
+
+            row = {"context": context, "spans": spans, "answers": answer_texts, "uuid": qa["id"]}
+            wfp_eval.write(json.dumps(row) + '\n')
+
+    rfp.close()
+    wfp_eval.close()
+    if data_type != "train":
+        wfp.close()
+    #shuffle wfp
+    return
 
 
-def get_embedding(counter, data_type, limit=-1, emb_file=None, size=None, vec_size=None, token2idx_dict=None):
+def get_embedding(counter, data_type, limit=20000, emb_file=None, size=None, vec_size=None, token2idx_dict=None):
     print("Generating {} embedding...".format(data_type))
     embedding_dict = {}
     filtered_elements = [k for k, v in counter.items() if v > limit]
+
+    assert vec_size is not None
+    for token in filtered_elements:
+            embedding_dict[token] = [np.random.normal(
+                scale=0.01) for _ in range(vec_size)]
+        print("{} tokens have corresponding embedding vector".format(
+            len(filtered_elements)))
+
     if emb_file is not None:
         assert size is not None
-        assert vec_size is not None
         with open(emb_file, "r", encoding="utf-8") as fh:
             for line in tqdm(fh, total=size):
                 array = line.split()
@@ -97,7 +123,6 @@ def get_embedding(counter, data_type, limit=-1, emb_file=None, size=None, vec_si
         print("{} / {} tokens have corresponding {} embedding vector".format(
             len(embedding_dict), len(filtered_elements), data_type))
     else:
-        assert vec_size is not None
         for token in filtered_elements:
             embedding_dict[token] = [np.random.normal(
                 scale=0.01) for _ in range(vec_size)]
@@ -118,7 +143,16 @@ def get_embedding(counter, data_type, limit=-1, emb_file=None, size=None, vec_si
     return emb_mat, token2idx_dict
 
 
-def build_features(config, examples, data_type, out_file, word2idx_dict, char2idx_dict, is_test=False):
+def build_features(config, data_type, word2idx_dict, char2idx_dict, is_test=False):
+
+    if data_type == "dev":
+        out_file = config.dev_record_file
+        rfp = open(config.dev_examples_file, 'r')
+    elif data_type == "test":
+        out_file = config.test_record_file
+        rfp = open(config.test_examples_file, 'r')
+    else:
+        exit(1)
 
     para_limit = config.test_para_limit if is_test else config.para_limit
     ques_limit = config.test_ques_limit if is_test else config.ques_limit
@@ -132,7 +166,8 @@ def build_features(config, examples, data_type, out_file, word2idx_dict, char2id
     total = 0
     total_ = 0
     meta = {}
-    for example in tqdm(examples):
+    for line in tqdm(rfp):
+        example = json.loads(line)
         total_ += 1
 
         if filter_func(example, is_test):
@@ -191,6 +226,7 @@ def build_features(config, examples, data_type, out_file, word2idx_dict, char2id
     print("Build {} / {} instances of features in total".format(total, total_))
     meta["total"] = total
     writer.close()
+    rfp.close()
     return meta
 
 
@@ -203,12 +239,9 @@ def save(filename, obj, message=None):
 
 def prepro(config):
     word_counter, char_counter = Counter(), Counter()
-    train_examples, train_eval = process_file(
-        config.train_file, "train", word_counter, char_counter)
-    dev_examples, dev_eval = process_file(
-        config.dev_file, "dev", word_counter, char_counter)
-    test_examples, test_eval = process_file(
-        config.test_file, "test", word_counter, char_counter)
+    #process_file(config, "train", word_counter, char_counter)
+    process_file(config, "dev", word_counter, char_counter)
+    #process_file(config, "test", word_counter, char_counter)
 
     word_emb_file = config.fasttext_file if config.fasttext else config.glove_word_file
     char_emb_file = config.glove_char_file if config.pretrained_char else None
@@ -219,29 +252,27 @@ def prepro(config):
     if os.path.isfile(config.word2idx_file):
         with open(config.word2idx_file, "r") as fh:
             word2idx_dict = json.load(fh)
-    word_emb_mat, word2idx_dict = get_embedding(word_counter, "word", emb_file=word_emb_file,
-                                                size=config.glove_word_size, vec_size=config.glove_dim, token2idx_dict=word2idx_dict)
+    word_emb_mat, word2idx_dict = get_embedding(
+        word_counter, "word", emb_file=word_emb_file,size=config.glove_word_size, 
+        vec_size=config.glove_dim, token2idx_dict=word2idx_dict)
 
     char2idx_dict = None
     if os.path.isfile(config.char2idx_file):
         with open(config.char2idx_file, "r") as fh:
             char2idx_dict = json.load(fh)
     char_emb_mat, char2idx_dict = get_embedding(
-        char_counter, "char", emb_file=char_emb_file, size=char_emb_size, vec_size=char_emb_dim, token2idx_dict=char2idx_dict)
+        char_counter, "char", emb_file=char_emb_file, size=char_emb_size, 
+        vec_size=char_emb_dim, token2idx_dict=char2idx_dict)
 
-    build_features(config, train_examples, "train",
-                   config.train_record_file, word2idx_dict, char2idx_dict)
-    dev_meta = build_features(config, dev_examples, "dev",
-                              config.dev_record_file, word2idx_dict, char2idx_dict)
-    test_meta = build_features(config, test_examples, "test",
-                               config.test_record_file, word2idx_dict, char2idx_dict, is_test=True)
+    dev_meta = build_features(config, "dev", word2idx_dict, char2idx_dict)
+    #test_meta = build_features(config, "test", word2idx_dict, char2idx_dict, is_test=True)
 
     save(config.word_emb_file, word_emb_mat, message="word embedding")
     save(config.char_emb_file, char_emb_mat, message="char embedding")
-    save(config.train_eval_file, train_eval, message="train eval")
-    save(config.dev_eval_file, dev_eval, message="dev eval")
-    save(config.test_eval_file, test_eval, message="test eval")
-    save(config.dev_meta, dev_meta, message="dev meta")
+
     save(config.word2idx_file, word2idx_dict, message="word2idx")
     save(config.char2idx_file, char2idx_dict, message="char2idx")
-    save(config.test_meta, test_meta, message="test meta")
+
+    save(config.dev_meta, dev_meta, message="dev meta")
+    #save(config.test_meta, test_meta, message="test meta")
+
