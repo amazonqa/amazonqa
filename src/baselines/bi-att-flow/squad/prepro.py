6e4efdc0ee0798fd4062a3fc23d9a5114ec52db5
Ubuntu <ubuntu@ip-172-31-38-213.us-west-2.compute.internal>
2018-12-02 02:44:59 +0000

    bidaf

--- src/baselines/bi-att-flow/squad/prepro.py
+++ src/baselines/bi-att-flow/squad/prepro.py
@@ -18,17 +18,17 @@ def main():
 
 def get_args():
     parser = argparse.ArgumentParser()
-    home = os.path.expanduser("~")
-    source_dir = os.path.join(home, "data", "squad")
-    target_dir = "data/squad"
-    glove_dir = os.path.join(home, "data", "glove")
+    home = os.path.expanduser("../../../")
+    source_dir = os.path.join(home, "data")
+    target_dir = "data"
+    glove_dir = os.path.join(home, "data")
     parser.add_argument('-s', "--source_dir", default=source_dir)
     parser.add_argument('-t', "--target_dir", default=target_dir)
     parser.add_argument('-d', "--debug", action='store_true')
     parser.add_argument("--train_ratio", default=0.9, type=int)
-    parser.add_argument("--glove_corpus", default="6B")
+    parser.add_argument("--glove_corpus", default="840B")
     parser.add_argument("--glove_dir", default=glove_dir)
-    parser.add_argument("--glove_vec_size", default=100, type=int)
+    parser.add_argument("--glove_vec_size", default=300, type=int)
     parser.add_argument("--mode", default="full", type=str)
     parser.add_argument("--single_path", default="", type=str)
     parser.add_argument("--tokenizer", default="PTB", type=str)
@@ -37,19 +37,6 @@ def get_args():
     parser.add_argument("--split", action='store_true')
     # TODO : put more args here
     return parser.parse_args()
-
-
-def create_all(args):
-    out_path = os.path.join(args.source_dir, "all-v1.1.json")
-    if os.path.exists(out_path):
-        return
-    train_path = os.path.join(args.source_dir, "train-v1.1.json")
-    train_data = json.load(open(train_path, 'r'))
-    dev_path = os.path.join(args.source_dir, "dev-v1.1.json")
-    dev_data = json.load(open(dev_path, 'r'))
-    train_data['data'].extend(dev_data['data'])
-    print("dumping all data ...")
-    json.dump(train_data, open(out_path, 'w'))
 
 
 def prepro(args):
@@ -121,8 +108,8 @@ def prepro_each(args, data_type, start_ratio=0.0, stop_ratio=1.0, out_name="defa
     if not args.split:
         sent_tokenize = lambda para: [para]
 
-    source_path = in_path or os.path.join(args.source_dir, "{}-v1.1.json".format(data_type))
-    source_data = json.load(open(source_path, 'r'))
+    source_path = in_path or os.path.join(args.source_dir, "{}-qar_squad_all.jsonl".format(data_type))
+    rfp = open(source_path, 'r')
 
     q, cq, y, rx, rcx, ids, idxs = [], [], [], [], [], [], []
     cy = []
@@ -130,88 +117,93 @@ def prepro_each(args, data_type, start_ratio=0.0, stop_ratio=1.0, out_name="defa
     answerss = []
     p = []
     word_counter, char_counter, lower_word_counter = Counter(), Counter(), Counter()
-    start_ai = int(round(len(source_data['data']) * start_ratio))
-    stop_ai = int(round(len(source_data['data']) * stop_ratio))
-    for ai, article in enumerate(tqdm(source_data['data'][start_ai:stop_ai])):
-        xp, cxp = [], []
-        pp = []
-        x.append(xp)
-        cx.append(cxp)
-        p.append(pp)
-        for pi, para in enumerate(article['paragraphs']):
-            # wordss
-            context = para['context']
-            context = context.replace("''", '" ')
-            context = context.replace("``", '" ')
-            xi = list(map(word_tokenize, sent_tokenize(context)))
-            xi = [process_tokens(tokens) for tokens in xi]  # process tokens
-            # given xi, add chars
-            cxi = [[list(xijk) for xijk in xij] for xij in xi]
-            xp.append(xi)
-            cxp.append(cxi)
-            pp.append(context)
+    #start_ai = int(round(len(source_data['data']) * start_ratio))
+    #stop_ai = int(round(len(source_data['data']) * stop_ratio))
+    pi = 0
+    ai = 0
+    xp, cxp = [], []
+    pp = []
+    x.append(xp)
+    cx.append(cxp)
+    p.append(pp)
 
-            for xij in xi:
-                for xijk in xij:
-                    word_counter[xijk] += len(para['qas'])
-                    lower_word_counter[xijk.lower()] += len(para['qas'])
-                    for xijkl in xijk:
-                        char_counter[xijkl] += len(para['qas'])
+    for line in tqdm(rfp):
+        para = json.loads(line)
+        context = para['context']
+        context = context.replace("''", '" ')
+        context = context.replace("``", '" ')
+        xi = list(map(word_tokenize, sent_tokenize(context)))
+        xi = [process_tokens(tokens) for tokens in xi]  # process tokens
+        # given xi, add chars
+        cxi = [[list(xijk) for xijk in xij] for xij in xi]
+        xp.append(xi)
+        cxp.append(cxi)
+        pp.append(context)
 
-            rxi = [ai, pi]
-            assert len(x) - 1 == ai
-            assert len(x[ai]) - 1 == pi
-            for qa in para['qas']:
-                # get words
-                qi = word_tokenize(qa['question'])
-                cqi = [list(qij) for qij in qi]
-                yi = []
-                cyi = []
-                answers = []
-                for answer in qa['answers']:
-                    answer_text = answer['text']
-                    answers.append(answer_text)
-                    answer_start = answer['answer_start']
-                    answer_stop = answer_start + len(answer_text)
-                    # TODO : put some function that gives word_start, word_stop here
-                    yi0, yi1 = get_word_span(context, xi, answer_start, answer_stop)
-                    # yi0 = answer['answer_word_start'] or [0, 0]
-                    # yi1 = answer['answer_word_stop'] or [0, 1]
-                    assert len(xi[yi0[0]]) > yi0[1]
-                    assert len(xi[yi1[0]]) >= yi1[1]
-                    w0 = xi[yi0[0]][yi0[1]]
-                    w1 = xi[yi1[0]][yi1[1]-1]
-                    i0 = get_word_idx(context, xi, yi0)
-                    i1 = get_word_idx(context, xi, (yi1[0], yi1[1]-1))
-                    cyi0 = answer_start - i0
-                    cyi1 = answer_stop - i1 - 1
-                    # print(answer_text, w0[cyi0:], w1[:cyi1+1])
-                    assert answer_text[0] == w0[cyi0], (answer_text, w0, cyi0)
-                    assert answer_text[-1] == w1[cyi1]
-                    assert cyi0 < 32, (answer_text, w0)
-                    assert cyi1 < 32, (answer_text, w1)
+        for xij in xi:
+            for xijk in xij:
+                word_counter[xijk] += len(para['qas'])
+                lower_word_counter[xijk.lower()] += len(para['qas'])
+                for xijkl in xijk:
+                    char_counter[xijkl] += len(para['qas'])
 
-                    yi.append([yi0, yi1])
-                    cyi.append([cyi0, cyi1])
+        rxi = [ai, pi]
+        assert len(x) - 1 == ai
+        assert len(x[ai]) - 1 == pi
+        for qa in para['qas']:
+            # get words
+            qi = word_tokenize(qa['question'])
+            cqi = [list(qij) for qij in qi]
+            yi = []
+            cyi = []
+            answers = []
+            for answer in qa['answers']:
+                answer_text = answer['text']
+                answers.append(answer_text)
+                answer_start = answer['answer_start']
+                answer_stop = answer_start + len(answer_text)
+                # TODO : put some function that gives word_start, word_stop here
+                yi0, yi1 = get_word_span(context, xi, answer_start, answer_stop)
+                # yi0 = answer['answer_word_start'] or [0, 0]
+                # yi1 = answer['answer_word_stop'] or [0, 1]
+                assert len(xi[yi0[0]]) > yi0[1]
+                assert len(xi[yi1[0]]) >= yi1[1]
+                w0 = xi[yi0[0]][yi0[1]]
+                w1 = xi[yi1[0]][yi1[1]-1]
+                i0 = get_word_idx(context, xi, yi0)
+                i1 = get_word_idx(context, xi, (yi1[0], yi1[1]-1))
+                cyi0 = answer_start - i0
+                cyi1 = answer_stop - i1 - 1
+                # print(answer_text, w0[cyi0:], w1[:cyi1+1])
+                assert answer_text[0] == w0[cyi0], (answer_text, w0, cyi0)
+                assert answer_text[-1] == w1[cyi1]
+                assert cyi0 < 32, (answer_text, w0)
+                assert cyi1 < 32, (answer_text, w1)
 
-                for qij in qi:
-                    word_counter[qij] += 1
-                    lower_word_counter[qij.lower()] += 1
-                    for qijk in qij:
-                        char_counter[qijk] += 1
+                yi.append([yi0, yi1])
+                cyi.append([cyi0, cyi1])
 
-                q.append(qi)
-                cq.append(cqi)
-                y.append(yi)
-                cy.append(cyi)
-                rx.append(rxi)
-                rcx.append(rxi)
-                ids.append(qa['id'])
-                idxs.append(len(idxs))
-                answerss.append(answers)
+            for qij in qi:
+                word_counter[qij] += 1
+                lower_word_counter[qij.lower()] += 1
+                for qijk in qij:
+                    char_counter[qijk] += 1
 
-            if args.debug:
-                break
+            q.append(qi)
+            cq.append(cqi)
+            y.append(yi)
+            cy.append(cyi)
+            rx.append(rxi)
+            rcx.append(rxi)
+            ids.append(qa['id'])
+            idxs.append(len(idxs))
+            answerss.append(answers)
+
+        if args.debug:
+            break
+
+        pi += 1
+
 
     word2vec_dict = get_word2vec(args, word_counter)
     lower_word2vec_dict = get_word2vec(args, lower_word_counter)
