a1e1ee5b9ac8307c9299782c7340f86d09105a26
Nitish Kulkarni <ni4lgi@gmail.com>
2018-12-09 23:42:32 +0000

    Correct span keys

--- src/evaluation/human_eval/create_human_predictions_span-based.py
+++ src/evaluation/human_eval/create_human_predictions_span-based.py
@@ -2,6 +2,15 @@ import sys
 import json
 import numpy as np
 
+SPAN_KEYS = [
+    'answers_sentence_ir',
+    'answers_sentence_bleu2',
+    'answers_sentence_bleu4',
+    'answers_snippet_spans_bleu2',
+    'answers_snippet_spans_bleu4',
+    'answers_snippet_spans_rouge',
+]
+
 def read_answers_from_full(input_filename):
     all_answers = []
     print('Reading input file')
@@ -12,26 +21,22 @@ def read_answers_from_full(input_filename):
             except json.JSONDecodeError:
                 raise Exception('\"%s\" is not a valid json' % line)
             qar = qar['qas'][0]
-            spans = [a['text'] for a in qar['answers'] if a['text'] != '']
+            spans = {}
+            for key in SPAN_KEYS:
+                spans[key] = [a['text'] for a in qar[key] if a['text'] != '']
             all_answers.append((spans, qar['human_answers']))
     return all_answers
 
 def create_ref_and_pred(all_answers, ref_filename, prediction_filename):
     print('Writing files')
-    with open(ref_filename + '_all_spans.jsonl', 'w', encoding='utf-8') as fp_ref:
-        with open(prediction_filename + '_all_spans.jsonl', 'w', encoding='utf-8') as fp_pred:
-            for qid, (spans, answers) in enumerate(all_answers):
-                if len(spans) > 0 and len(answers) > 0:
-                    for i, span in enumerate(spans):
-                        fp_pred.write('%s\n' % json.dumps({'qid': qid, 'answers': [span]}))
-                        fp_ref.write('%s\n' % json.dumps({'qid': qid, 'answers': answers}))
-
-    with open(ref_filename + '_first_span.jsonl', 'w', encoding='utf-8') as fp_ref:
-        with open(prediction_filename + '_first_span.jsonl', 'w', encoding='utf-8') as fp_pred:
-            for qid, (spans, answers) in enumerate(all_answers):
-                if len(spans) > 0 and len(answers) > 0:
-                    fp_pred.write('%s\n' % json.dumps({'qid': qid, 'answers': [spans[0]]}))
-                    fp_ref.write('%s\n' % json.dumps({'qid': qid, 'answers': answers}))
+    for span_key in SPAN_KEYS:
+        with open('%s_spans_%s.jsonl' % (ref_filename, span_key), 'w', encoding='utf-8') as fp_ref:
+            with open('%s_spans_%s.jsonl' % (prediction_filename, span_key), 'w', encoding='utf-8') as fp_pred:
+                for qid, (spans, answers) in enumerate(all_answers):
+                    if len(spans[span_key]) > 0 and len(answers) > 0:
+                        for i, span in enumerate(spans[span_key]):
+                            fp_pred.write('%s\n' % json.dumps({'qid': qid, 'answers': [span]}))
+                            fp_ref.write('%s\n' % json.dumps({'qid': qid, 'answers': answers}))
 
     with open(ref_filename + '_all_answers.jsonl', 'w', encoding='utf-8') as fp_ref:
         with open(prediction_filename + '_all_answers.jsonl', 'w', encoding='utf-8') as fp_pred:
@@ -42,19 +47,13 @@ def create_ref_and_pred(all_answers, ref_filename, prediction_filename):
                         fp_pred.write('%s\n' % json.dumps({'qid': qid, 'answers': [answer]}))
                         fp_ref.write('%s\n' % json.dumps({'qid': qid, 'answers': ref_answers}))
 
-    with open(ref_filename + '_first_answer.jsonl', 'w', encoding='utf-8') as fp_ref:
-        with open(prediction_filename + '_first_answer.jsonl', 'w', encoding='utf-8') as fp_pred:
-            for qid, answers in enumerate(all_answers):
-                if len(answers) > 1:
-                    fp_pred.write('%s\n' % json.dumps({'qid': qid, 'answers': [answers[0]]}))
-                    fp_ref.write('%s\n' % json.dumps({'qid': qid, 'answers': answers[1:]}))
+def main():
+    input_filename = sys.argv[1]
+    output_filename = sys.argv[2]
 
-def main(input_filename, ref_filename, prediction_filename):
+    ref_filename, prediction_filename = '%s_ref' % output_filename, '%s_pred' % output_filename
     all_answers = read_answers_from_full(input_filename)
     create_ref_and_pred(all_answers, ref_filename, prediction_filename)
 
 if __name__ == '__main__':
-    input_filename = sys.argv[1]
-    output_filename = sys.argv[2]
-
-    main(input_filename, '%s_ref' % output_filename, '%s_pred' % output_filename)
+    main()
